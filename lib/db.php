ad99649d6d4b57024d6b2b93d9de1b6ec6f033f9
Jomin Joseph <81305046+jominjoseph@users.noreply.github.com>
2021-03-24 08:17:59 -0400

    Update db.php

--- lib/db.php
+++ lib/db.php
@@ -1,16 +1,15 @@
-<?php 
+<?php
 
 function getDB(){
   global $db;
   if(!isset($db)){
-    
-    include (  "account.php"     ) ; 
+    include ("account.php") ; 
     $db = mysqli_connect($hostname,$username,$password, $project );
-    if (mysqli_connect_errno()){
+    if (mysqli_connect_errno()){      
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
-	      exit();
-     }
-   print "You have successfully connected to MySQL database.<br>";
+        exit();
+    }
+    print "You have successfully connected to MySQL database.<br>";
     mysqli_select_db( $db, $project );
   }
   return $db;
