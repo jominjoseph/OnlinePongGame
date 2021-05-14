2b2551bcaae52ed46ec2c68fe88da2ea966da9fd
Jomin Joseph <81305046+jominjoseph@users.noreply.github.com>
2021-05-13 23:13:00 -0400

    Update nav.php

--- Login/nav.php
+++ Login/nav.php
@@ -1,19 +1,23 @@
-<?php 
+<?php
  session_set_cookie_params([
             'lifetime' => 60*60,
-            'path' => '/~jj496/IT202',
+            'path' => '/~jj496/IT202/',
             'domain' => $_SERVER['HTTP_HOST'],
             'secure' => true,
             'httponly' => true,
             'samesite' => 'lax'
         ]);
 session_start();
-//echo var_export(session_get_cookie_params(), true); 
-$sidvalue = session_id(); 
+//echo var_export(session_get_cookie_params(), true);
+$sidvalue = session_id();
 //echo "<br>Your session id: " . $sidvalue . "<br>";
 require(__DIR__ . "/../lib/myFunctions.php");
 ?>
 <link rel="stylesheet" href="styles.css">
+<script
+  src="https://code.jquery.com/jquery-3.6.0.js"
+  integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="
+  crossorigin="anonymous"></script>
 <ul class="nav">
 <?php if(!is_logged_in()):?>
 <li><a href="authenticate.php">Login</a></li>
@@ -21,6 +25,8 @@ require(__DIR__ . "/../lib/myFunctions.php");
 <?php endif;?>
 <?php if(is_logged_in()):?>
 <li><a href="home.php">Home</a></li>
+<li><a href="profile.php">Profile</a></li>
 <li><a href="logout.php">Logout</a></li>
+<li><a href="../game/game.php">Game</a></li>
 <?php endif;?>
 </ul>
